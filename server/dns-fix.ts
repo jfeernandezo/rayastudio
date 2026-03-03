4f6ccf016d699b47dc4fc3d9677c8ea38b5479ba
jfeernandezo <jfeernandeso@gmail.com>
2026-03-03 02:26:43 -0300

    fix: simplificar dns-fix para compatibilidade com Node 24

--- server/dns-fix.ts
+++ server/dns-fix.ts
@@ -1,21 +1,3 @@
 import dns from "dns";
 
 dns.setDefaultResultOrder("ipv4first");
-
-const resolver = new dns.Resolver();
-resolver.setServers(["1.1.1.1", "8.8.8.8"]);
-
-const nativeLookup = dns.lookup;
-
-(dns as any).lookup = (hostname: string, options: any, callback: any) => {
-    const cb = typeof options === "function" ? options : callback;
-    const opts = typeof options === "function" ? {} : options;
-
-    resolver.resolve4(hostname, (err, addresses) => {
-        if (!err && addresses?.length) {
-            cb(null, addresses[0], 4);
-        } else {
-            (nativeLookup as any)(hostname, opts, cb);
-        }
-    });
-};
